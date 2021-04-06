117664fda23c70499f562e68db4310c0b85e0535
Hashir Sajjad <hashir_sajjad@hotmail.com>
2021-04-06 12:55:17 +0500

    Changes in project after adding code

--- src/com/company/SampleClass.java
+++ src/com/company/SampleClass.java
@@ -3,5 +3,6 @@ package com.company;
 public class SampleClass {
     SampleClass(){
         System.out.println("Sample Class for Github");
+        System.out.println("Changes after adding new code");
     }
 }
